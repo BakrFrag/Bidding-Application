71048e56b50f0867db2b87fe0c69fb06dcb8fe10
bk <bakr.frag@gmail.com>
2026-01-03 09:30:33 +0200

    include routing for websockets url patterns

--- bids/routing.py
+++ bids/routing.py
@@ -1,3 +1,8 @@
+from django.urls import re_path
+from . import consumers
+
 websocket_urlpatterns = [
-    # Define your WebSocket URL patterns here
+    # Captures the bid_id as a keyword argument for the consumer
+    # URL: ws://127.0.0.1:8000/ws/bid/1/
+    re_path(r'ws/bid/(?P<bid_id>\d+)/$', consumers.BidConsumer.as_asgi()),
 ]
